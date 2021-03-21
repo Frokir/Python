57de5d64566810398445ca0ab9424df3942fdceb
Frokir <perepletchikov.o@gmail.com>
2021-03-21 20:36:04 +0200

    Задание 3 сделано

--- Lesson 6 work 3.py
+++ Lesson 6 work 3.py
@@ -3,28 +3,24 @@ class Worker:
         self.name = name
         self.surname = surname
         self.position = position
-        self.__income = {'wage': 0, 'bonus': 0}
+        self._income = {'wage': 0, 'bonus': 0}
 
-    def incomer(self, wage, bonus):
-        self.__income['wage'] = int(wage)
-        self.__income['bonus'] = int(bonus)
+    def set_incom(self, wage, bonus):
+        self._income['wage'] = wage
+        self._income['bonus'] = bonus
+
 
 class Position(Worker):
-    def __init__(self, name='', surname='', position=''):
-        self.__income = {'wage': 0, 'bonus': 0}
-        super().__init__(name, surname, position,)
-
-
     def get_full_name(self):
         return f'{self.name} {self.surname}'
 
     def get_total_income(self):
-        return self.__income['wage'] * self.__income['bonus']
+        return self._income['wage'] + self._income['bonus']
 
-worker = Position(name='Oleg', surname='Perepl')
-worker.position = 'Boss'
-worker.incomer(10000, 5000)
-print(worker.get_total_income())
 
-print(worker.get_full_name())
-
+worker_1 = Position(name='Oleg', surname='Perepl')
+worker_1.position = 'Boss'
+worker_1.set_incom(10000, 5000)
+print(worker_1.get_full_name())
+print(worker_1.get_total_income())
+print(worker_1.position)
